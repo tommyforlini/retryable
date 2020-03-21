29c68047d068c3a3c088591e6619a68ddd48e60a
Tommy Forlini <tommy.forlini@gmail.com>
2020-03-20 20:35:55 -0400

    rename config to client and provide a default func

--- retryable_test.go
+++ retryable_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestSuccess(t *testing.T) {
 
-	retryable := &Config{
+	retryable := &Client{
 		maxAttempts: 2,
 		delay:       time.Second * 1,
 	}
@@ -22,9 +22,18 @@ func TestSuccess(t *testing.T) {
 	assert.NoError(t, err)
 }
 
+func TestSuccessWithDefault(t *testing.T) {
+
+	err := NewClient().Try(func() error {
+		return nil
+	})
+
+	assert.NoError(t, err)
+}
+
 func TestSuccessExternalizedFunction(t *testing.T) {
 
-	retryable := &Config{
+	retryable := &Client{
 		maxAttempts: 2,
 		delay:       time.Second * 1,
 	}
@@ -42,7 +51,7 @@ func MyCoolFunction() error {
 
 func TestMaxAttemptsExhausted(t *testing.T) {
 
-	retryable := &Config{
+	retryable := &Client{
 		maxAttempts: 2,
 		delay:       time.Second * 1,
 	}
@@ -59,9 +68,29 @@ func TestMaxAttemptsExhausted(t *testing.T) {
 
 }
 
+func TestMaxAttemptsExhaustedWithDefaultsDelayOverride(t *testing.T) {
+
+	retryable := NewClient()
+	retryable.delay = time.Second * 1
+
+	err := retryable.Try(func() error {
+		return errors.New("It failed")
+	})
+
+	assert.Error(t, err)
+	expectedErrorFormat := `Retryable failed:
+#1: It failed
+#2: It failed
+#3: It failed
+#4: It failed
+#5: It failed`
+	assert.Equal(t, expectedErrorFormat, err.Error(), "retry error messages")
+
+}
+
 func TestUnrecoverableError(t *testing.T) {
 
-	retryable := &Config{
+	retryable := &Client{
 		maxAttempts: 2,
 		delay:       time.Second * 1,
 	}
